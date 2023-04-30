d9502004c2300ab1e12b61eac2bf52f4bb8167df
FinkParker <parkeririshgod@gmail.com>
2023-04-30 17:53:17 -0400

    can list stuff in list

--- main.js
+++ main.js
@@ -7,15 +7,42 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
 
+const ip = '10.0.0.135'
+const port = 6469
 
+
+function clearList(){
+    for (i in list){
+        list.pop(i)
+        console.log(list)
+    }
+}
+
+
+var list = []
 
 
 app.get('/', function(req,res){
-    res.render('index.ejs')
+    res.render('index.ejs', {
+        list: list
+    })
+})
+
+app.post('/', function(req,res){
+    let listItem = {
+        item: req.body.item
+    }
+
+    list.push(listItem.item)
+    console.log(list)
+
+    res.redirect('/')
 })
 
 
-app.listen(6469, function(){
-    console.log("server running")
+
+
+app.listen(port, ip, function(){
+    console.log(`${ip}:${port}`)
 })
 
